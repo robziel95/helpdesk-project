0f3ac864b385e8048c2f4cef7cddfa318f9b3d62
Robert Zielinski <zielinskirob95@gmail.com>
2018-11-05 13:06:29 +0100

    added unique validator

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
   name: {type: String, required: true},
   surname: {type: String, required: true},
+  //unique is not a validator
+  //npm install --save mongoose-unique-validator was used for validation
   email: {type: String, required: true, unique: true },
   password: {type: String, required: true}
 });
+
+//validate unique fields
+userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('User', userSchema);
 //create model from schema
